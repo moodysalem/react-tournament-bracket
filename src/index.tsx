007faa0fd3ed05b5b30feec993b64fd25925f3ce
Moody Salem <salmahmo@amazon.com>
2018-05-04 11:33:51 -0400

    add model to exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import Bracket from './components/Bracket';
 import BracketGame from './components/BracketGame';
 import BracketGenerator from './components/BracketGenerator';
+import * as Model from './components/model';
 
 export {
   Bracket,
   BracketGame,
-  BracketGenerator
+  BracketGenerator,
+  Model
 };
