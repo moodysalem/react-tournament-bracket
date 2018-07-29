b17bc3aa11255be07efd379696e2c2d1dff86119
Moody Salem <salmahmo@amazon.com>
2018-07-29 18:21:13 -0400

    Updates

--- src/index.jsx
+++
@@ -1,4 +0,0 @@
-export Bracket from './components/Bracket';
-export BracketGame from './components/BracketGame';
-export BracketGenerator from './components/BracketGenerator';
-export GameShape from './components/GameShape';
